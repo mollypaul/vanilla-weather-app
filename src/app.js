7c0faefd449952c32ef0bc16e80c95b661b3a35e
mollypaul <79339865+mollypaul@users.noreply.github.com>
2021-05-05 09:05:39 -0500

    Added search engine form

--- src/app.js
+++ src/app.js
@@ -32,8 +32,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "f515e1078d21bb51c506cea818e9747e";
-let city = "Olathe"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+    let apiKey = "f515e1078d21bb51c506cea818e9747e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
