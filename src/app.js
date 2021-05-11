9399ca681811e3e6d83d4af07b8092fc1e45320b
mollypaul <79339865+mollypaul@users.noreply.github.com>
2021-05-10 23:01:39 -0500

    Added forecast function to JavaScript

--- src/app.js
+++ src/app.js
@@ -13,6 +13,40 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function(day) {
+    forecastHTML =
+    forecastHTML +
+    `
+    <div class="col-4">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max"> 18° </
+        span>
+        <span class="weather-forecast-temperature-min"> 12° </
+        span>
+      </div>
+    </div>
+
+  `;
+  })
+  
+  
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
+
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -66,6 +100,7 @@ function displayCelsiusTemperature(event) {
 let celsiusTemperature = null;
 
 
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -76,3 +111,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
